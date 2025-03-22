bae5fb3b0b7491daa10ed17e00c6b0e4f69d04c2
kongarapavankumar <kongarapavankumar@gmail.com>
2025-03-22 19:05:39 +0530

    product_page

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-$(function() {
-    $("h1").text( 'InventoFlow');
+$(document).ready(function() {
+    $('.sidebar li').click(function() {
+        $('.sidebar li').removeClass('active');
+        $(this).addClass('active');
+        $('.content-section').hide();
+        $('#' + $(this).data('content')).show();
+    });
+
+    $('#categoryFilter').change(function() {
+        filterTable();
+    });
+
+    $('#searchInput').on('input', function() {
+        filterTable();
+    });
+
+    function filterTable() {
+        var category = $('#categoryFilter').val().toLowerCase();
+        var searchText = $('#searchInput').val().toLowerCase();
+
+        $('#productTableBody tr').each(function() {
+            var rowCategory = $(this).find('td:eq(2)').text().toLowerCase();
+            var rowName = $(this).find('td:eq(1)').text().toLowerCase();
+
+            if ((category === '' || rowCategory === category) &&
+                (searchText === '' || rowName.includes(searchText))) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
 });
-    
