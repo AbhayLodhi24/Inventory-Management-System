76fb6faa96437d8fe8e10da7fd68b5cf74c09b0b
Sathvik Bitra <sathvikbitra16@gmail.com>
2025-03-23 22:00:45 +0530

    Added Users module in admin part

---
+++ Users_Admin/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    //Search functionality
+    $('#search').on('keyup', function() {
+        const value = $(this).val().toLowerCase();
+        $('tbody tr').filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        });
+    });
+
+    // Delete Row
+    $(document).on('click', '.deleteButton', function () {
+        $(this).closest('tr').remove();
+
+        // Recalculate UIDs after deletion
+        $(".uid").each(function (index) {
+            $(this).text(index + 1);
+        });
+    });
+
+    // Add new row
+    $('#userForm').submit(function(event){
+        event.preventDefault();
+        const name = $('#name').val();
+        const email = $('#mail').val();
+        const pwd = $('#pwd').val();
+        const address = $('#address').val();
+        const role = $('#role').val();
+        let uid = $('.uid').last().text();
+        let new_uid = parseInt(uid)+1;
+        if(!name || !email || !pwd || !address || !role){
+            alert("Please fill all fields");
+        }
+        else{
+            const newRow = `
+            <tr>
+                <td class="name">${new_uid}</td>
+                <td class="category">${name}</td>
+                <td class="supplier">${email}</td>
+                <td class="price">${role}</td>
+                <td>
+                    <button class="btn btn-link deleteButton text-danger btn-no-underline">Delete</button>
+                </td>
+            </tr>`;
+            $('tbody').append(newRow);
+
+            // Clear form inputs
+            $("#userForm")[0].reset();
+        }
+    });
+
+
+});
