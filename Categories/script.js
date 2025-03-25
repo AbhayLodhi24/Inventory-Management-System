f46d8a320301bfe3e086b74d1555da9fda84a49d
Prabhath-Y <yvprabhath@gmail.com>
2025-03-25 13:41:12 +0530

    Added logout functioanlity

--- Categories/script.js
+++ Categories/script.js
@@ -73,3 +73,30 @@ $(document).ready(function () {
         });
     });
 });
+
+
+ // Handle Logout functionality
+    // Populate modal with user details when the logout button is clicked
+    $('#logoutButton').click(function (e) {
+        e.preventDefault(); // Prevent default action for the logout link
+      //  $('#modalUserName').text(loginName); // Populate modal with user name
+      //  $('#modalUserEmail').text(loginEmail); // Populate modal with user email
+ 
+        // Show the logout confirmation modal
+        $('#logoutModal').modal('show');
+    });
+ 
+    // Cancel Logout Functionality
+    $('#cancelLogout').click(function () {
+        $('#logoutModal').modal('hide'); // Simply hide the modal without any further action
+    });
+ 
+    // Handle "Logout" confirmation in the modal
+    $('#confirmLogout').click(function () {
+        $('#logoutModal').modal('hide'); // Hide the modal
+        console.log("User has logged out."); // Log the action
+         // Notify the user (can be replaced with actual logout logic)
+        // Redirect to the login page or perform other logout actions
+        window.location.href = '../Login/login.html'; // Replace with your actual login page
+    });
+ 
